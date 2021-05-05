8e092c28d9db0272623c7a72fb61b368b4cd30a2
Don Heppner <donald.heppner@gmail.com>
2021-05-04 22:46:36 -0400

    Created a utility to help calibrate the sensors

---
+++ calibrator.py
@@ -0,0 +1,33 @@
+import math
+import time
+import threading
+import logging
+import json
+import uuid
+
+try:
+    from Board import Board
+except Exception as e:
+    logging.info(f'Could not load Board: {e}')
+    from MockBoard import Board
+
+from Cooker import Thermistor
+
+def main():
+    b = Board()
+    thermistors = [Thermistor(b, p) for p in range(8) if p % 2 == 0]
+
+    while True:
+        readings = ''
+        for t in thermistors:
+            r = t.reading()
+            readings += f'{t.pin}: R:{r.resistance:6.2f}  '
+
+        print(f'\r{readings}', end='')
+        time.sleep(.1)
+
+
+if __name__ == '__main__':
+    print('Starting Meat-Pi Calibration')
+    print('Press Ctrl-C to exit')
+    main()
